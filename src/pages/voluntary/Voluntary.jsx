f3e2306616b0046b8aba73ce157bf48898f71ac3
RicardoFrancoDelgado <rf041555@gmail.com>
2025-12-10 19:33:51 -0300

    :rocket: feat: create forms container

--- src/pages/voluntary/Voluntary.jsx
+++ src/pages/voluntary/Voluntary.jsx
@@ -22,6 +22,37 @@ const Voluntary = () => {
           />
         </div>
       </section>
+      <section className={s.formularioVoluntario}>
+        <div className={s.formulario}>
+          <h3>Inscrição para Voluntários</h3>
+
+          <form action="" className={s.form}>
+            <fieldset>
+              <legend>Dados Pessoais</legend>
+              <div className={s.nomeEmail}>
+                <input
+                  type="text"
+                  placeholder="Seu Nome *"
+                  required
+                  className={s.nome}
+                />
+                <input type="email" placeholder="Seu Email *" required />
+              </div>
+              <input type="tel" placeholder="Seu Telefone *" required />
+            </fieldset>
+
+            <fieldset className={s.mensagem}>
+              <legend>Mensagem Adicional</legend>
+              <textarea placeholder="Conte-nos porque você quer ser voluntario..."></textarea>
+            </fieldset>
+
+            <fieldset className={s.button}>
+              <legend>Entraremos em contato para mais informações</legend>
+              <button type="submit">Enviar Inscrição</button>
+            </fieldset>
+          </form>
+        </div>
+      </section>
     </>
   );
 };
