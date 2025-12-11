697b8404bfed2d6360a46505784fe6fe26fd62db
RicardoFrancoDelgado <rf041555@gmail.com>
2025-12-11 12:03:30 -0300

    :rocket: feat: finish responsive layout

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,22 +1,41 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import s from "./header.module.scss";
 import PrimaryButton from "../primary-button/PrimaryButton";
 
 const Header = () => {
+  const [open, setOpen] = useState(false);
+
   return (
     <header className={s.header}>
       <div className={s.headerTitle}>
         <img src="/coracao.png" />
-        <Link className={s.linkHome} to="/">
+        <Link className={s.linkHome} to="/" onClick={() => setOpen(false)}>
           Médicos & Dentistas
         </Link>
       </div>
-      <nav className={s.headerLinks}>
-        <Link className={s.linkHome} to="/">
+
+      <button
+        className={s.burger}
+        aria-label={open ? "Fechar menu" : "Abrir menu"}
+        aria-expanded={open}
+        onClick={() => setOpen((v) => !v)}
+      >
+        <span className={s.burgerLine} />
+        <span className={s.burgerLine} />
+        <span className={s.burgerLine} />
+      </button>
+
+      <nav className={`${s.headerLinks} ${open ? s.open : ""}`}>
+        <Link className={s.linkHome} to="/" onClick={() => setOpen(false)}>
           Home
         </Link>
-        <PrimaryButton to="/voluntary" title="Seja voluntário" />
+        <span onClick={() => setOpen(false)}>
+          <PrimaryButton to="/voluntary" title="Seja voluntário" />
+        </span>
       </nav>
+
+      {open && <div className={s.backdrop} onClick={() => setOpen(false)} />}
     </header>
   );
 };
