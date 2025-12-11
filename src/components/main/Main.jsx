2c0952f1f545abfec0af1c90c5ad2b8c102de43c
RicardoFrancoDelgado <rf041555@gmail.com>
2025-12-11 11:49:01 -0300

    :rocket: fix adjust responsive layout

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -5,7 +5,7 @@ import s from "./main.module.scss";
 const Main = () => {
   return (
     <section className={s.mainContainer}>
-      <section>
+      <section className={s.textSection}>
         <div className={s.mainBudget}>
           <img src="/Frame.png" alt="icone de um aparelho de médico" />
           <p>Saúde para todos</p>
@@ -26,8 +26,8 @@ const Main = () => {
         </div>
       </section>
 
-      <section>
-        <img src="/doutor.png" alt="Imagem de um médico sorrindo" />
+      <section className={s.imageSection}>
+        <img src="/doutor.png" alt="Imagem de um médico sorrindo" className={s.doctorImage}/>
       </section>
     </section>
   );
