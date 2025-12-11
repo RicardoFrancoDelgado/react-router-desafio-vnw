ee493cab9bdc32b67b57739a83a5a8dc451c1adf
RicardoFrancoDelgado <rf041555@gmail.com>
2025-12-11 11:23:26 -0300

    :rocket: fix: change px to rem

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,3 @@
-import Footer from "../../components/footer/Footer";
 import Impact from "../../components/impact/Impact";
 import Main from "../../components/main/Main";
 import Mission from "../../components/mission/Mission";
